3dc58fc11ddef5d492b60bedd791c3a37d3bd563
cadibemma <chikaadibemma@live.com>
2020-07-07 19:18:32 -0400

    Fix: Modified zscore formula

--- Statistics/ZScore.py
+++ Statistics/ZScore.py
@@ -1,16 +1,14 @@
-from Calculator.Addition import addition
 from Calculator.Subtraction import subtraction
-from Calculator.Square import square
 from Calculator.Division import division
-# from Statistics.PopulationMean import populationmean
-# from Statistics.StandardDeviation import stddev
+from Statistics.PopulationMean import populationmean
+from Statistics.StandardDeviation import stddev
 
 
-# def zscore(num):
-#     zmean = populationmean(num)
-#     sd = stddev(num)
-#     zlist = []
-#     for x in num:
-#         z = round(((x - zmean) / sd), 6)
-#         zlist.append(z)
-#     return zlist
+def z_score(num):
+    z_mean = populationmean(num)
+    sd = stddev(num)
+    zlist = []
+    for x in num:
+        z = round(division(subtraction(x, z_mean), sd), 6)
+        zlist.append(z)
+    return zlist
