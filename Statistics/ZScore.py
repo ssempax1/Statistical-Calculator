66ce395091ac85c1664a36f5369bf96667f43c01
cadibemma <chikaadibemma@live.com>
2020-07-07 17:59:33 -0400

    Feature: Created method for z score

---
+++ Statistics/ZScore.py
@@ -0,0 +1,16 @@
+from Calculator.Addition import addition
+from Calculator.Subtraction import subtraction
+from Calculator.Square import square
+from Calculator.Division import division
+# from Statistics.PopulationMean import populationmean
+# from Statistics.StandardDeviation import stddev
+
+
+# def zscore(num):
+#     zmean = populationmean(num)
+#     sd = stddev(num)
+#     zlist = []
+#     for x in num:
+#         z = round(((x - zmean) / sd), 6)
+#         zlist.append(z)
+#     return zlist
