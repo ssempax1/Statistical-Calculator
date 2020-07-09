d7b85e3f1cca0a8b9598f266cc896440e3525f20
ian <isk5@njit.edu>
2020-07-09 00:25:20 -0400

    added Test Data file

--- Statistics/Sampler.py
+++ Statistics/Sampler.py
@@ -2,5 +2,7 @@ from random import random
 
 
 def sampler(data, sample_size):
-    random_values = random.sample(data, k=sample_size)
+    random_values = []
+    for i in range(sample_size):
+        random_values.append(float(data[i]))
     return random_values
